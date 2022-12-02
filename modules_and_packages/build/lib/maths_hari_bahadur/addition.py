6f7cbb1775b05326dd7cc54aef1e6761f7c514f0
Suyog Subedi <subedisuyog555@gmail.com>
2022-12-02 22:15:03 +0545

    removed some files

--- modules_and_packages/build/lib/maths_hari_bahadur/addition.py
+++
@@ -1,4 +0,0 @@
-def addition(*args):
-    if len(args)==0:
-        return "Can't add empty things, can I ?"
-    return(sum(args))
--- modules_and_packages/build/lib/maths_hari_bahadur/multiplication.py
+++
@@ -1,8 +0,0 @@
-def multiply(*args):
-    if len(args)==0:
-        return "Can't multiply empty things, can I ?"
-    value=1
-    for num in args:
-        value = num * value
-    return value
-    
