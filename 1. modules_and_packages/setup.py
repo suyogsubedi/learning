060399a76e4769838580c6e57f8002f3e75f195d
Suyog Subedi <subedisuyog555@gmail.com>
2022-12-04 20:13:37 +0545

    updated setup.py

--- 1. modules_and_packages/setup.py
+++ 1. modules_and_packages/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
-setup(name='cool_package',
+setup(name='mathss',
 version='0.1',
-description='Adds and multiplies',
+description='Adds, multiplies, checks for pallindrome',
 url='#',
 author='Hari Bahadur',
 author_email='maharibahadur@ihihihi.com',
 license='MIT',
-packages=['maths_hari_bahadur'],
+packages=['mathss'],
 zip_safe=False)
